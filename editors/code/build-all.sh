0f397a16a9dddd96f51ae75e8a23189adab4f016
Uros Mrkobrada <uros.mrkobrada@gmail.com>
2026-06-11 13:04:42 +0200

    target more than 5 architectures

--- editors/code/build-all.sh
+++ editors/code/build-all.sh
@@ -1,6 +1,6 @@
 
-sh ./build.sh jinja-lsp-darwin-arm64 jinja-lsp-darwin-arm64 darwin-arm64
-sh ./build.sh jinja-lsp-darwin-x64 jinja-lsp-darwin-x64 darwin-x64
-sh ./build.sh jinja-lsp-linux-x64 jinja-lsp-linux-x64 linux-x64
-sh ./build.sh jinja-lsp-windows-arm64.exe jinja-lsp-windows-arm64 win32-arm64
-sh ./build.sh jinja-lsp-windows-x64.exe jinja-lsp-windows-x64 win32-x64
+sh ./build.sh jinja-lsp-aarch64-apple-darwin jinja-lsp-aarch64-apple-darwin darwin-arm64
+sh ./build.sh jinja-lsp-x86_64-apple-darwin jinja-lsp-x86_64-apple-darwin darwin-x64
+sh ./build.sh jinja-lsp-x86_64-unknown-linux-gnu jinja-lsp-x86_64-unknown-linux-gnu linux-x64
+sh ./build.sh jinja-lsp-aarch64-pc-windows-msvc.exe jinja-lsp-aarch64-pc-windows-msvc win32-arm64
+sh ./build.sh jinja-lsp-x86_64-pc-windows-msvc.exe jinja-lsp-x86_64-pc-windows-msvc win32-x64
